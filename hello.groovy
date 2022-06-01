e25f02d5b906de2e04dd7cb6e80bc04550f35a58
Varshil Soni <Varshil.Soni@einfochips.com>
2022-06-01 18:37:42 +0530

    first commit

---
+++ hello.groovy
@@ -0,0 +1,7 @@
+class Demo {  
+    static void main(args) {  
+        print("Welcome to Javatpoint tutorial on Groovy... ")  
+
+        print("hello varshil , this is to ckeck the git is working")
+    }  
+}  
